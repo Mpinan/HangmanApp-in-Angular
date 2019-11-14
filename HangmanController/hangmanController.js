be0334a2873ae63d5cd3d4aa8aafee327ab7b3c0
Student <student@Makerss-MacBook-Pro.local>
2019-11-14 17:20:47 +0000

    Added scopes variables for the hangman game

--- HangmanController/hangmanController.js
+++ HangmanController/hangmanController.js
@@ -1,3 +1,18 @@
-app.controller("AppController", function($scope) {
-    
+app.controller("AppController", function ($scope) {
+    const words = ["rat", "cat", "bat"];
+    $scope.wrongGuess = [];
+    $scope.rightGuess = [];
+    $scope.guesses = 6;
+    $scope.displayWord = "";
+    $scope.input = {
+        letter: ""
+    }
+
+
+
+    selectWord(){
+        let index = Math.round(Math.random() * words.length)
+        return words[index];
+    }
+
 })
