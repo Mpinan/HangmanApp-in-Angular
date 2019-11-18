f87cd7d9dc3c292c07d6b22b9cd76d5debbb5d36
Student <student@makerss-mbp.home>
2019-11-18 17:47:28 +0000

    Fixed some typos

--- HangmanController/hangmanController.js
+++ HangmanController/hangmanController.js
@@ -29,12 +29,14 @@ app.controller("AppController", function ($scope) {
 
 
     let selectedWord = function () {
-        let index = Math.round(Math.random() * words.length);
+        let index = Math.floor(Math.random() * words.length);
         return words[index];
     }
 
     $scope.letterChosen = function () {
+        console.log("I am here again")
         for (let i = 0; i < $scope.rightGuess.length; i++) {
+            
             if ($scope.rightGuess[i].toLowerCase() == $scope.input.letter.toLowerCase()); {
                 $scope.input.letter = "";
                 return;
@@ -43,6 +45,7 @@ app.controller("AppController", function ($scope) {
         }
 
         for (let i = 0; i < $scope.wrongGuess.length; i++) {
+            
             if ($scope.wrongGuess[i].toLowerCase() == $scope.input.letter.toLowerCase()); {
                 $scope.input.letter = "";
                 return;
@@ -53,16 +56,25 @@ app.controller("AppController", function ($scope) {
         let correct = false;
         for (let i = 0; i < selectedWord.length; i++) {
             if (selectedWord[i].toLowerCase() == $scope.input.letter.toLowerCase()) {
-                $scope.displayWord = $scope.displayWord.slice(0, i) + $scope.input.letter.toLowerCase() + $scope.displayWord.slice(i + 1)
+                $scope.displayWord = $scope.displayWord.slice(0, 1) + $scope.input.letter.toLowerCase() + $scope.displayWord.slice(i + 1)
                 correct = true;
             }
         }
+
         if (correct) {
+            console.log("I am correct")
             $scope.rightGuess.push($scope.input.letter.toLowerCase());
         } else {
+            console.log("I am incorrect")
             $scope.wrongGuess.push($scope.input.letter.toLowerCase());
         }
         $scope.input.letter = ""
+        if($scope.guesses == 0){
+            alert("You Lost!")
+        }
+        if($scope.displayWord.indexOf('*') == - 1){
+            alert("You Won!")
+        }
     }
 
     newGame()
